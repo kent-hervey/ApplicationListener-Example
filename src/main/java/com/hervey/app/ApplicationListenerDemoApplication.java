0ba89cae275b3ab18de014fd9b574f0f8ebce309
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-07-22 10:05:19 -0500

    first commit

---
+++ src/main/java/com/hervey/app/ApplicationListenerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hervey.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationListenerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationListenerDemoApplication.class, args);
+	}
+
+}
