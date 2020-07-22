bdf0afb6350354d756ff815c9544287ce7c68046
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-07-22 12:45:05 -0500

    added name and number of pages to Book

---
+++ src/main/java/com/hervey/app/models/Book.java
@@ -0,0 +1,108 @@
+package com.hervey.app.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="books")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private Integer numberPages;
+	
+	
+	public Book() {
+		
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Integer getNumberPages() {
+		return numberPages;
+	}
+
+
+	public void setNumberPages(Integer numberPages) {
+		this.numberPages = numberPages;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((numberPages == null) ? 0 : numberPages.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (numberPages == null) {
+			if (other.numberPages != null)
+				return false;
+		} else if (!numberPages.equals(other.numberPages))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", numberPages=" + numberPages + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
